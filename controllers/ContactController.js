482df0970fb27cdb4da9c76b19351ca78b7a8705
bdange <benjamin.dange@gmail.com>
2019-06-05 14:32:10 -0400

    current work

--- controllers/ContactController.js
+++ controllers/ContactController.js
@@ -1,8 +1,10 @@
 const Contact = require("../db/models").Contact;
+const inquirer = require("inquirer");
 
 module.exports = class ContactController {
   constructor() {
     this.contacts = [];
+
     this.addContactQuestions = [
       {
         type: "input",
@@ -29,10 +31,63 @@ module.exports = class ContactController {
         }
       }
     ];
+
+    this.searchQuestions = [
+      {
+        type: "input",
+        name: "name",
+        message: "Name of contact to search - ",
+        validate(val) {
+          return val !== "";
+        }
+      }
+    ];
+
+    this.showContactQuestions = [
+      {
+        type: "list",
+        name: "selected",
+        message: "Please choose from an option below: ",
+        choices: ["Delete contact", "Main menu"]
+      }
+    ];
+
+    this.deleteConfirmQuestions = [
+      {
+        type: "confirm",
+        name: "confirmation",
+        message: "are you sure you want to delete this contact?"
+      }
+    ];
   }
 
   addContact(name, phone, email) {
     return Contact.create({ name, phone, email });
+  }
+
+  delete(id) {
+    return Contact.destroy({
+      where: { id }
+    });
+  }
+
+  delete(contact) {
+    inquirer
+      .prompt(this.book.deleteConfirmQuestions)
+      .then(answer => {
+        if (answer.confirmation) {
+          this.book.delete(contact.id);
+          console.log("contact deleted!");
+          this.main();
+        } else {
+          console.log("contact not deleted");
+          this.showContact(contact);
+        }
+      })
+      .catch(err => {
+        console.log(err);
+        this.main();
+      });
   }
 
   getContacts() {
@@ -45,6 +100,38 @@ module.exports = class ContactController {
         return contact;
       }
     }
+    return null;
+  }
+
+  search(name) {
+    return Contact.findOne({
+      where: { name }
+    });
+  }
+
+  binarySearch(contacts, target) {
+    let min = 0;
+    let max = contacts.length - 1;
+    let mid;
+
+    while (min <= max) {
+      // #1
+      mid = Math.floor((min + max) / 2);
+      let currentContact = contacts[mid];
+
+      // #2
+      if (currentContact.name > target) {
+        //target is before element at mid, eliminate upper bound
+        max = mid - 1;
+      } else if (currentContact.name < target) {
+        //target is after element at mid, eliminate lower bound
+        min = mid + 1;
+      } else {
+        //element found, return it
+        return contacts[mid];
+      }
+    }
+
     return null;
   }
 };
--- spec/contact_controller_spec.js
+++ spec/contact_controller_spec.js
@@ -1,23 +1,19 @@
 const ContactController = require("../controllers/ContactController");
+
 const sequelize = require("../db/models/index").sequelize;
 
 describe("ContactController", () => {
   beforeEach(done => {
     this.book = new ContactController();
 
-    // #1
     sequelize
       .sync({ force: true })
       .then(res => {
         done();
       })
-      .catch(err => {
+      .catch(error => {
         done();
       });
-  });
-
-  it("should be defined", () => {
-    expect(ContactController).toBeDefined();
   });
 
   describe("#addContact()", () => {
@@ -30,7 +26,7 @@ describe("ContactController", () => {
           expect(contact.email).toBe("alice@gmail.com");
           done();
         })
-        .catch(err => {
+        .catch(error => {
           done();
         });
     });
@@ -44,12 +40,11 @@ describe("ContactController", () => {
           expect(contacts.length).toBe(0);
           done();
         })
-        .catch(err => {
-          console.log(err);
+        .catch(error => {
+          console.log(error);
           done();
         });
     });
-
     it("should return an array of contacts when contacts are available", done => {
       this.book
         .addContact("Alice", "001-101-1010", "alice@example.com")
@@ -59,14 +54,13 @@ describe("ContactController", () => {
             done();
           });
         })
-        .catch(err => {
-          console.log(err);
+        .catch(error => {
+          console.log(error);
           done();
         });
     });
   });
-
-  describe("#iterativeSearch()", () => {
+  describe("search methods", () => {
     const zelda = ["Zelda Smith", "000-100-111", "zelda@nintendo.com"];
     const snake = ["Solid Snake", "100-100-100", "snake@konami.com"];
     const magus = ["Magus Johnson", "101-010-101", "magus@squaresoft.com"];
@@ -76,40 +70,18 @@ describe("ContactController", () => {
       "allow@guerrilla-games.com"
     ];
 
-    it("should return null when called with an empty array", () => {
-      expect(this.book.iterativeSearch([], "Alloy")).toBeNull();
-    });
-
-    it("should return null when contact is not found", done => {
-      this.book.addContact(...zelda).then(() => {
-        this.book
-          .getContacts()
-          .then(contacts => {
-            expect(
-              this.book.iterativeSearch(contacts, "Alloy Rodriguez")
-            ).toBeNull();
-            done();
-          })
-          .catch(err => {
-            console.log(err);
-            done();
-          });
+    describe("#iterativeSearch()", () => {
+      it("should return null when called with an empty array", () => {
+        expect(this.book.iterativeSearch([], "Alloy")).toBeNull();
       });
-    });
-
-    it("should return the contact if found", done => {
-      this.book.addContact(...alloy).then(() => {
-        this.book.addContact(...magus).then(() => {
+      it("should return null when contact is not found", done => {
+        this.book.addContact(...zelda).then(() => {
           this.book
             .getContacts()
             .then(contacts => {
-              let contact = this.book.iterativeSearch(
-                contacts,
-                "Magus Johnson"
-              );
-              expect(contact.name).toBe("Magus Johnson");
-              expect(contact.phone).toBe("101-010-101");
-              expect(contact.email).toBe("magus@squaresoft.com");
+              expect(
+                this.book.iterativeSearch(contacts, "Alloy Rodriguez")
+              ).toBeNull();
               done();
             })
             .catch(err => {
@@ -117,6 +89,167 @@ describe("ContactController", () => {
               done();
             });
         });
+      });
+
+      it("should return the contact if found", done => {
+        this.book.addContact(...alloy).then(() => {
+          this.book.addContact(...magus).then(() => {
+            this.book
+              .getContacts()
+              .then(contacts => {
+                let contact = this.book.iterativeSearch(
+                  contacts,
+                  "Magus Johnson"
+                );
+                expect(contact.name).toBe("Magus Johnson");
+                expect(contact.phone).toBe("101-010-101");
+                expect(contact.email).toBe("magus@squaresoft.com");
+                done();
+              })
+              .catch(err => {
+                console.log(err);
+                done();
+              });
+          });
+        });
+      });
+    });
+    describe("#binarySearch()", () => {
+      function sort(contacts) {
+        return contacts.sort((a, b) => {
+          if (a.name > b.name) return 1;
+          else if (a.name < b.name) return -1;
+          else return 0;
+        });
+      }
+
+      it("should return null when called with an empty array", () => {
+        expect(this.book.binarySearch([], "Alloy Rodriguez")).toBeNull();
+      });
+
+      it("should return null when contact is not found", done => {
+        this.book.addContact(...zelda).then(() => {
+          this.book
+            .getContacts()
+            .then(contacts => {
+              expect(
+                this.book.binarySearch(sort(contacts), "Alloy Rodriguez")
+              ).toBeNull();
+              done();
+            })
+            .catch(err => {
+              console.log(err);
+              done();
+            });
+        });
+      });
+
+      it("should return the contact if found", done => {
+        this.book.addContact(...alloy).then(() => {
+          this.book.addContact(...magus).then(() => {
+            this.book.addContact(...zelda).then(() => {
+              this.book.addContact(...snake).then(() => {
+                this.book
+                  .getContacts()
+                  .then(contacts => {
+                    let contact = this.book.binarySearch(
+                      sort(contacts),
+                      "Magus Johnson"
+                    );
+                    expect(contact.name).toBe("Magus Johnson");
+                    expect(contact.phone).toBe("101-010-101");
+                    expect(contact.email).toBe("magus@squaresoft.com");
+                    done();
+                  })
+                  .catch(err => {
+                    console.log(err);
+                    done();
+                  });
+              });
+            });
+          });
+        });
+      });
+    });
+    describe("#search()", () => {
+      it("should return null when a contact was not found", done => {
+        this.book.addContact(...zelda).then(() => {
+          this.book
+            .search("Solid Snake")
+            .then(contact => {
+              expect(contact).toBeNull();
+              done();
+            })
+            .catch(err => {
+              console.log(err);
+              done();
+            });
+        });
+      });
+
+      it("should return the contact when found", done => {
+        this.book.addContact(...snake).then(() => {
+          this.book
+            .search("Solid Snake")
+            .then(contact => {
+              expect(contact).not.toBeNull();
+              expect(contact.name).toBe("Solid Snake");
+              expect(contact.phone).toBe("100-100-100");
+              expect(contact.email).toBe("snake@konami.com");
+              done();
+            })
+            .catch(err => {
+              console.log(err);
+              done();
+            });
+        });
+      });
+    });
+    describe("#delete()", () => {
+      it("should not remove any contacts that do not match the ID passed", done => {
+        this.book
+          .addContact("Rick Deckard", "000-000-000", "null@null.com")
+          .then(() => {
+            this.book.getContacts().then(contacts => {
+              expect(contacts[0].name).toBe("Rick Deckard");
+              expect(contacts.length).toBe(1);
+              this.book.delete(99).then(() => {
+                this.book
+                  .getContacts()
+                  .then(contacts => {
+                    expect(contacts.length).toBe(1);
+                    done();
+                  })
+                  .catch(err => {
+                    console.log(err);
+                    done();
+                  });
+              });
+            });
+          });
+      });
+
+      it("should remove the contact that matches the ID passed", done => {
+        this.book
+          .addContact("Rick Deckard", "000-000-000", "null@null.com")
+          .then(contact => {
+            this.book.getContacts().then(contacts => {
+              expect(contacts[0].name).toBe("Rick Deckard");
+              expect(contacts.length).toBe(1);
+              this.book.delete(contact.id).then(() => {
+                this.book
+                  .getContacts()
+                  .then(contacts => {
+                    expect(contacts.length).toBe(0);
+                    done();
+                  })
+                  .catch(err => {
+                    console.log(err);
+                    done();
+                  });
+              });
+            });
+          });
       });
     });
   });
